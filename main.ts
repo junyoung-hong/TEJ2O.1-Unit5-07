3bc37953a872e2df0c3cede145782900b652061d
junyoung-hong <junyoung.hong@stu.ocsb.ca>
2026-04-01 13:07:05 -0400

    not done

--- main.ts
+++ main.ts
@@ -1,8 +1,32 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2026 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Junyoung
+ * Created on: April 1ST!!!!!
+ * This program check the distance between objects and choose what motors do
 */
+//variables
+let distanceObject : number = 0
 
-basic.showString('Hello, World!')
+// setup
+basic.showIcon(IconNames.Happy)
+distanceObject = sonar.ping(DigitalPin.P1, DigitalPin.P2, PingUnit.MicroSeconds)
+
+// loop forever
+while (true) {
+    if (input.buttonIsPressed(Button.A) == true) {
+        // turn the motor 180 degrees
+        basic.showIcon(IconNames.Yes)
+        robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B0)
+        robotbit.StepperTurn(robotbit.Steppers.M2, robotbit.Turns.T1B0)
+        basic.showIcon(IconNames.Happy)
+    }
+
+    if (input.buttonIsPressed(Button.B) == true) {
+        // move car forwards and backwards
+        basic.showIcon(IconNames.Yes)
+        robotbit.StpCarMove(10, 48)
+        basic.pause(500)
+        robotbit.StpCarMove(-10, 48)
+        basic.showIcon(IconNames.Happy)
+    }
+}
