30d582a9d2d8df049e1d9e5489dcef7aa217aebd
junyoung-hong <junyoung.hong@stu.ocsb.ca>
2026-06-17 20:43:19 -0400

    rewrited the code and changed to bettwer version

--- main.ts
+++ main.ts
@@ -4,40 +4,49 @@
  * Created on: April 1ST!!!!!
  * This program check the distance between objects and choose what motors do
 */
-// show happy face
+
+// variables
+let distanceObject: number = 0
+let go: number = 0 
+
+// set up
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
-// make sure the car is stopped before initialization
 robotbit.StpCarMove(0, 48)
 
 // when the "A" button is clicked
 input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
     basic.showIcon(IconNames.Yes)
+    go = 1 
 
-    // repeats over and over again
-    while (true) {
-
-        // get the distance using the sonar
+    // repeat go
+    while (go == 1) {
         const distance = sonar.ping(
-            DigitalPin.P1, // trigger
-            DigitalPin.P2, // echo
-            PingUnit.Centimeters,
+            DigitalPin.P1,
+            DigitalPin.P2,
+            PingUnit.Centimeters
         )
 
-        // if distance is below 10 cm
         if (distance > 0 && distance <= 10) {
-            basic.clearScreen()
-            basic.showString(distance.toString() + ' cm')
+            robotbit.StpCarMove(0, 48)
+            basic.showIcon(IconNames.No)
+            robotbit.StpCarMove(-10, 48)
+            basic.pause(1000)
+            robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B4)
+            basic.pause(500)
             basic.showIcon(IconNames.Yes)
-            robotbit.StpCarMove(0, 48) // stop the car
-            basic.pause(1000)
-            robotbit.StpCarMove(-10, 48) // reverse 10 cm
-            basic.pause(1000)
-            robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B4) // turn 90 degrees
         } else {
             robotbit.StpCarMove(1, 48)
         }
+        basic.pause(50)
     }
+})
+
+// when the "B" button is clicked
+input.onButtonPressed(Button.B, function () {
+    go = 0 
+    robotbit.StpCarMove(0, 48)
+    basic.showIcon(IconNames.Happy)
 })
